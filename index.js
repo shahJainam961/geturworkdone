fcf75f9d7383bf3bbc421597f99558be36b300d2
Jainam Shah <shahjainam961@gmail.com>
2020-07-27 15:48:32 +0530

    only household

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express()
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
-let cat=0;
+
 let household = []
-let officework = []
+
 app.get("/",function(req,res){
     
     let today = new Date();
@@ -20,43 +20,18 @@ app.get("/",function(req,res){
     }
     day = today.toLocaleDateString("en-US",options)
 
-    if(cat==0){
+    
         res.render("list",{
             dayName: day,
             newDo: household,
-            cat: cat
         })
-    }
-    else{
-        res.render("list",{
-            dayName: day,
-            newDo: officework,
-            cat: cat   
-        })
-    }
     
 })
 
 app.post("/",function (req,res) {
     let item = req.body.newItem
 
-    if(cat==0){
-        household.push(item);
-    }
-    else{
-        officework.push(item)
-    }
-
-    res.redirect("/")
-})
-
-app.post("/ho",function(req,res){
-    cat = 0;
-    res.redirect("/")
-})
-
-app.post("/ow",function(req,res){
-    cat = 1;
+    household.push(item);
     res.redirect("/")
 })
 
